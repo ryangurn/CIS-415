0ac30b44cb8735a2d3d09e07c335a6ea070d9e6e
Ryan Gurnick <ryangurnick@gmail.com>
2020-04-05 23:08:58 -0700

    working on execute() and fixed typo in input.txt

--- projects/project 1/main.c
+++ projects/project 1/main.c
@@ -1,5 +1,6 @@
+// stupid error: warning: implicit declaration of function ‘getline’; did you mean ‘getlogin’?
 #define  _GNU_SOURCE
-
+#include <ctype.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -17,8 +18,40 @@
 #define true (1==1)
 #define false (1==0)
 #define debug true
-#define file_input stdin
-#define file_output stdout
+#define interactive_input stdin
+#define interactive_output stdout
+#define file_output "output.txt"
+const char file_delimiter[2] = " ";
+
+// helper functions
+char *left_trim(char *s){ while(isspace(*s)) s++; return s; }
+char *right_trim(char *s){ char *back = s + strlen(s); while(isspace(*--back)); *(back+1) = '\0'; return s; }
+char *trim(char *s){ return right_trim(left_trim(s));  }
+
+// execute()
+// Purpose: the purpose of this function is to ensure that execution of the commands are done consistently
+//          since there is overlap between file and interactive mode, this will ensure the code stays DRY
+// DRY Reference: https://en.wikipedia.org/wiki/Don%27t_repeat_yourself
+// Design Rationale: the rationale for this function is mainly to ensure that code is staying DRY and that
+//                   will also have payoff's for debugging/performance/etc.
+int execute(char *line)
+{
+  if (debug) { printf("<<< COMPILING TOKENS\n"); }
+  char *tokenize_initial = strtok(line, ";");
+
+  while(tokenize_initial != NULL)
+  {
+
+    if (debug) { printf("<<< token(%s)\n", trim(tokenize_initial)); }
+    // execute the commands
+
+    tokenize_initial = strtok(NULL, ";");
+  }
+
+  if (debug) { printf("<<< FINISHING WITH TOKENS\n"); }
+
+  return 0;
+}
 
 // interactive()
 // Purpose: this function will run the interactive mode for the application
@@ -32,11 +65,11 @@ int interative()
   size_t len = 0;
 
   // clear the std out buffer
-  setbuf(file_output, NULL);
+  setbuf(interactive_output, NULL);
   while (true)
   {
     printf(">>> "); // print the input notation
-    getline(&input, &len, file_input); // get the input line and set size from standard in
+    getline(&input, &len, interactive_input); // get the input line and set size from standard in
 
     if (debug) { printf("<<<(%ld) %s\n", len, input); } // debug
 
@@ -50,12 +83,22 @@ int interative()
       return 1;
     }
 
+    // execute commands
+    execute(input); // execute that shit
   }
   return 0;
 }
 
+// file()
+// Args: * file - a string reference to the file that can be used and bassed.
+// Purpose: this function will run the file mode for the application
+// Design Rationale: ensure that all functionality for file mode is tightly coupled
 int file(char *file)
 {
+  // open the output file
+  // reference: http://www.decompile.com/cpp/faq/fopen_write_append.htm
+  FILE *o = fopen(file_output, "a+"); // append new information or create file if it does not exists
+
   // attempt to open the file based on str
   int exists = open(file, O_RDONLY);
   if (debug) { printf("<<< EXISTS: (%d)\n", exists); }
@@ -68,6 +111,27 @@ int file(char *file)
   }
 
   // file does exist
+  FILE *f = fopen(file, "r");
+  // something went wrong above
+  if(f == NULL)
+  {
+    if (debug) { printf("<<< WE SHOULD NOT GET HERE... RUN GDB\n"); }
+    printf("Error!: Unknown error occurred.\n");
+  }
+
+  // finally we can get the contents!
+  char *line = NULL;
+  size_t line_length = 0;
+  getline(&line, &line_length, f); // get the line the first time
+  do{
+    execute(line); // execute that shit
+  } while(getline(&line, &line_length, f) != -1); // iterate until we end
+
+  // close the files
+  fclose(f); // close the input file
+  fclose(o);
+
+
   return 0;
 }
 
