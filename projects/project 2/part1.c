188ca0170b97144ea1f8cac08dc9427a9ddb22da
Ryan Gurnick <ryangurnick@gmail.com>
2020-05-10 20:12:47 -0700

    project 2 part 1 still with some minor valgrind issues

--- projects/project 2/part1.c
+++ projects/project 2/part1.c
@@ -3,95 +3,101 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <string.h>
+#include <sys/syscall.h>
 #include <sys/types.h>
 #include <sys/wait.h>
 
+// globals
+#define true (1==1)
+#define false (1==0)
+
 int main(int argc, char const *argv[])
 {
-    setbuf(stdout, NULL); // clear the output buffer.
-    
-    char *line = NULL;
-    size_t len = 0;
-    
-    if (argc != 2) {
-        printf("Error!: Wrong command line arguments\n");
-        return 1;
+  // clear stdout
+  setbuf(stdout, NULL);
+
+  char *line = NULL;
+  size_t number;
+  size_t length = 0;
+  char *token;
+  char *arguements[10];
+  int i = 0;
+
+  FILE *in;
+
+  // check input parameters
+  if (argc == 1 || argc < 2) {
+    printf("Error: wrong command line arguments\n");
+    return 1;
+  }
+
+  // open the input file
+  in = fopen(argv[1], "r");
+  // check for input file errors
+  if (in == NULL) {
+    printf("Error: cannot open input file\n");
+    return 1;
+  }
+
+  int pc = 0;
+  // determine the number of lines (ie a program counter)
+  while ((number = getline(&line, &length, in)) != -1) {
+    pc++; // add to the program counter
+  }
+
+  fclose(in); // close the input just for a moment
+
+  in = fopen(argv[1], "r");
+  int j = 0;
+  pid_t pid[pc]; // setup the correct numbers of pid's
+  pid_t wait;
+  int waitStatus;
+
+  while ((number = getline(&line, &length, in)) != -1) {
+    i = 0; // set position back to zero for use again
+    for (; i < 10; i++) {
+      arguements[i] = NULL; // setup empty array with NULL ptrs
     }
-    
-    // open the input file using the argv parameter
-    FILE *input = fopen(argv[1], "r");
-    if (input == NULL)
-    {
-        printf("Error!: Unable to open input file");
-        return 1;
+
+    i = 0; // set position back to zero for use again
+    token = strtok(line, " "); // start parsing tokens
+    while (token != NULL) {
+      // remove new line from the end of the token
+      if (token[strlen(token)-1] == '\n') {
+        token[strlen(token)-1] = 0; // swap with a zero
+      }
+
+      arguements[i] = token; // assign to position in arr
+      token = strtok(NULL, " "); // move token forward
+      i++; // interate position in arr forward
     }
-    
-    // count the number of lines from the input
-    int count = 0;
-    while((l = getline(&line, &len, input) != -1)
-    {
-        count++;
+
+    pid[j] = fork(); // fork
+
+    if (pid[j] < 0) { // check for fork errors
+      exit(-1); // exit if needed
     }
-    
-    // setup an arguments arr
-    int args[5];
-    size_t args_len = 5;
-    for (int i = 0; i < args_len; i++)
-    {
-        args[i] = NULL; // init value
+
+    // execute child
+    if (pid[j] == 0) {
+      execvp(arguements[0], arguements);
+      printf("Error!: Invalid Executable\n");
+      exit(-1);
     }
-    
-    int fork_id = 0;
-    pid_t pid[count];
-    while((l = getline(&line, &len, input) != -1)
-    {
-        int ind = 0;
-        char *token = strtok(line, " ");
-        while (token != NULL)
-        {
-            // replace new lines with a zero
-            if (token[strlen(token)-1] == '\n')
-            {
-                token[strlen(token)-1] = 0; // replacement
-            }
-            
-            args[ind] = token; // put the token into the args arr
-            token = strtok(NULL, " "); // get the next token
-            ind++; // keep moving forward
-        }
-        
-        // time to start forking and check for errors
-        pid[fork_id] = fork();
-        if (pid[fork_id] < 0) 
-        {
-            exit(-1); // exit the process
-        }
-        
-        // check if the current process is a child
-        if (pid[fork_id] == 0)
-        {
-            execvp(args[0], args);
-            printf("\nError: Invalid Executable\n");
-            exit(-1);
-        }
-        
-        
-        fork_id++;
-    }
-    
-    pid_t wait;
-    int status;
-    for (int i = 0; i < count; i++)
-    {
-        status = waitpid(pid[i], &wait, WUNTRACED | WCONTINUED);
-    }
-    
-    // close the input file.
-    fclose(input);
-    
-    // free some stuff
-    free(line);
-    // TODO: ensure that we do not need to free anything else with valgrind once the VM is accessible.
-    
-    return 0;
+
+    j++; // iterate pid iterator
+  }
+
+  // wait
+  int waitIterator = 0;
+  for (waitIterator; waitIterator < pc; waitIterator++) {
+    wait = waitpid(pid[waitIterator], &waitStatus, WUNTRACED | WCONTINUED);
+  }
+
+  // free all the things
+  free(token);
+  free(line);
+  fclose(in);
+  return 1;
 }
